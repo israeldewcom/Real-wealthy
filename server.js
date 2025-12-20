6547e564ae0e186ddee4424441567a37d3758022
israeldewcom <haddockthor@gmail.com>
2025-12-20 17:31:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ config.allowedOrigins = [
   'http://localhost:3001',
   'https://rawwealthy.com',
   'https://www.rawwealthy.com',
-  'https://cozy-starship-cbc959.netlify.app/',
+  'https://kn-rawwealthy.vercel.app/',
   'https://kn-rawwealthy.vercel.app/',
   'https://real-wealthy-1.onrender.com'
 ].filter(Boolean);
